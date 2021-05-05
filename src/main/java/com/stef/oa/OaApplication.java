cbea686cd4ae7fd869dd3928ca66ad04accef9f8
feng <wanshaofeng001@hotmail.com>
2021-05-05 19:58:41 +0800

    集成nacos

--- src/main/java/com/stef/oa/OaApplication.java
+++ src/main/java/com/stef/oa/OaApplication.java
@@ -1,9 +1,10 @@
 package com.stef.oa;
 
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
+@EnableDiscoveryClient
 @SpringBootApplication
 public class OaApplication {
 
